af81fddeb78d6f21fb73b1e0fc42c5364c9f9ab1
Sebastian <amsebastian12@gmail.com>
2025-04-06 12:08:57 -0400

    Added collision, and did some house cleaning

--- webgl/SpookyForest/BasicGameObject.js
+++ webgl/SpookyForest/BasicGameObject.js
@@ -1,79 +1,85 @@
 class Transform
-		{
-			constructor()
-			{
-				this.forward = [0,0,1];
-				this.right = [1,0,0];
-				this.up = [0,1,0];
-			}
-		
-			doRotations(RotAngles)
-			{
-				this.xRot = [
-							[1,0,0,0],
-							[0,Math.cos(RotAngles[0]),-1*Math.sin(RotAngles[0]),0],
-							[0,Math.sin(RotAngles[0]),Math.cos(RotAngles[0]),0],
-							[0,0,0,1]
-						];		
-				this.yRot = [
-						[Math.cos(RotAngles[1]),0,Math.sin(RotAngles[1]),0],
-						[0,1,0,0],
-						[-1*Math.sin(RotAngles[1]),0,Math.cos(RotAngles[1]),0],
-						[0,0,0,1]	
-						];
-				this.zRot = [
-							[Math.cos(RotAngles[2]),-1*Math.sin(RotAngles[2]),0,0],
-							[Math.sin(RotAngles[2]),Math.cos(RotAngles[2]),0,0],
-							[0,0,1,0],
-							[0,0,0,1]
-						]
-				//this.forward = this.crossMultiply(xRot,[0,0,1,0]);		
-				this.forward = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[0,0,1,0])))
-				this.right = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[1,0,0,0])))
-				this.up = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[0,1,0,0])))
-			}			
-			crossMultiply(M,V)
-			{
-				console.log(M[0][3]);
-				console.log(V[3]);
-				var temp = [
-							M[0][0]*V[0]+M[0][1]*V[1]+M[0][2] * V[2]+ M[0][3]*V[3],
-							M[1][0]*V[0]+M[1][1]*V[1]+M[1][2] * V[2]+ M[1][3]*V[3],
-							M[2][0]*V[0]+M[2][1]*V[1]+M[2][2] * V[2]+ M[2][3]*V[3],
-							M[3][0]*V[0]+M[3][1]*V[1]+M[3][2] * V[2]+ M[3][3]*V[3]
-							]
-				console.log(temp);
-				return temp;
-			}
-			
-		}
+{
+	constructor()
+	{
+		this.forward = [0,0,1];
+		this.right = [1,0,0];
+		this.up = [0,1,0];
+	}
+
+	doRotations(RotAngles)
+	{
+		this.xRot = [
+					[1,0,0,0],
+					[0,Math.cos(RotAngles[0]),Math.sin(RotAngles[0]),0],
+					[0,-1*Math.sin(RotAngles[0]),Math.cos(RotAngles[0]),0],
+					[0,0,0,1]
+				];		
+		this.yRot = [
+				[Math.cos(RotAngles[1]),0,Math.sin(RotAngles[1]),0],
+				[0,1,0,0],
+				[-1*Math.sin(RotAngles[1]),0,Math.cos(RotAngles[1]),0],
+				[0,0,0,1]	
+				];
+		this.zRot = [
+					[Math.cos(RotAngles[2]),Math.sin(RotAngles[2]),0,0],
+					[-1*Math.sin(RotAngles[2]),Math.cos(RotAngles[2]),0,0],
+					[0,0,1,0],
+					[0,0,0,1]
+				]
+		//this.forward = this.crossMultiply(xRot,[0,0,1,0]);		
+		this.forward = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[0,0,1,0])))
+		this.right = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[1,0,0,0])))
+		this.up = this.crossMultiply(this.zRot,this.crossMultiply(this.yRot,this.crossMultiply(this.xRot,[0,1,0,0])))
+	}			
+	crossMultiply(M,V)
+	{
+		//console.log(M[0][3]);
+		//console.log(V[3]);
+		var temp = [
+					M[0][0]*V[0]+M[0][1]*V[1]+M[0][2] * V[2]+ M[0][3]*V[3],
+					M[1][0]*V[0]+M[1][1]*V[1]+M[1][2] * V[2]+ M[1][3]*V[3],
+					M[2][0]*V[0]+M[2][1]*V[1]+M[2][2] * V[2]+ M[2][3]*V[3],
+					M[3][0]*V[0]+M[3][1]*V[1]+M[3][2] * V[2]+ M[3][3]*V[3]
+					]
+		//console.log(temp);
+		return temp;
+	}
+	
+}
 
 
 class GameObject
 {
-	constructor() 
+	constructor(tCount, pType) 
 	{
 		this.loc = [0,0,0];
 		this.rot = [0,0,0];
 		this.isTrigger = false;
-		this.collissionRadius = 1.0;
+		this.cRadX = 1.0;
+		this.cRadY = 1.0;
+		this.cRadZ = 1.0;
 		this.velocity = [0,0,0];
 		this.angVelocity = [0,0,0];
 		this.name = "default";
 		this.id = 0;
 		this.prefab;
+		
+		this.triangleCount = tCount;
+		this.primitiveType = pType;
 		this.transform = new Transform();
 	}
 	
 	Move()
 	{
-		var tempP = [0,0,0]
-		for(var i =0; i< 3;i ++)
+		var tempP = [0,0,0];
+		for(var i =0; i < 3;i ++)
 		{
 			tempP[i] = this.loc[i];
 			tempP[i] += this.velocity[i];
 			this.rot[i] += this.angVelocity[i];
 		}
+		// If any of the rotation values reach 360 degrees (6.28319~), reset back to 0 degrees
 		if(!this.isTrigger)
 		{
 			var clear = true;
@@ -81,62 +87,75 @@ class GameObject
 			{
 				if(m.Solid[so] != this)
 				{
-					if(m.CheckCollision(tempP,this.collissionRadius,m.Solid[so].loc,m.Solid[so].collissionRadius))
+					if(m.CheckCollision(this.name,tempP, this.cRadX, this.cRadY, this.cRadZ, m.Solid[so].loc, m.Solid[so].cRadX, m.Solid[so].cRadY, m.Solid[so].cRadZ))
 					{
+						this.OnCollisionEnter(m.Solid[so])
+					
+						try
+						{
+							m.Solid[so].OnCollisionEnter(this);
+						}
+						catch{}
+					
 						clear = false;
 					}
 				}
 			} 
 			if(clear)
 			{
-			this.loc = tempP;
+				this.loc = tempP;
 			}
 		}
 		else
-		{
+		{  
 			this.loc = tempP;
+			// Check the trigger object against any solid objects
+			for(var so in m.Solid)
+			{
+				// If we already collided with the solid object earlier and it has a OnTriggerEnter then
+				// we can simply store it and use it here
+				if(m.CheckCollision(this.name,tempP, this.cRadX, this.cRadY, this.cRadZ, m.Solid[so].loc, m.Solid[so].cRadX, m.Solid[so].cRadY, m.Solid[so].cRadZ))
+				{
+					// If there is a detected collision then call the TriggerEnter function from the
+					// object thats listening for collisions, in this case the trigger object
+					this.OnTriggerEnter(m.Solid[so]);
+					try
+					{
+						m.Solid[so].OnTriggerEnter(this);
+					}
+					catch
+					{}
+				}
+			
+			}
+			// Now check trigger with trigger just in case
+			for(var to in m.Trigger)
+			{ 	//this should be correct. It is trying to check for trigger objects insted of solid objects
+				if(this != m.Trigger[to]){
+					if(m.CheckCollision(this.name,tempP, this.cRadX, this.cRadY, this.cRadZ, m.Trigger[to].loc, m.Trigger[to].cRadX, m.Trigger[to].cRadY, m.Trigger[to].cRadZ))
+					{
+						this.OnTriggerEnter(m.Trigger[to]);
+						try
+						{
+							m.Trigger[to].OnTriggerEnter(this);
+						}
+						catch
+						{}
+					}
+				}
+			}
 		}
 	}
 
+	OnCollisionEnter(other){}
+
+	OnTriggerEnter(other){}
 	
-	Update()
-	{
-		console.error(this.name +" update() is NOT IMPLEMENTED!");
-	}
+
 	Render(program)
 	{
-		console.error(this.name + " render() is NOT IMPLEMENTED!");
-	}	
-}
-class Ground extends GameObject
-{
-	constructor()
-	{
-		super();
-		this.buffer=gl.createBuffer();
-		this.colorBuffer = gl.createBuffer();
-		
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-		this.vertices =
-		[
-			-1000,0,-1000,0,1,0,
-			1000,0, -1000,0,1,0,
-			-1000,0,1000,0,1,0,
-			1000, 0,1000,0,1,0
-			
-		];
-	
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
-		this.loc=[0,0,0];
-		this.rot=[0,0,0];
-	}
-	Update()
-	{
-		//Do Nothing
-	}
-	Render(program)
-	{
-				var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+		var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
 		var size = 3;          // 2 components per iteration
 		var type = gl.FLOAT;   // the data is 32bit floats
@@ -162,22 +181,62 @@ class Ground extends GameObject
 		var thetaLoc = gl.getUniformLocation(program,'rotation');
 		gl.uniform3fv(thetaLoc,new Float32Array(this.rot));
 	 
-	 //var ibuffer = gl.createBuffer();
-	 //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,this.ibuffer);
-	 //gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint8Array(this.indexOrder),gl.STATIC_DRAW);
-	 //gl.drawElements(gl.TRIANGLES,this.indexOrder.length,gl.UNSIGNED_BYTE,0);
-	 gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+	 	//var ibuffer = gl.createBuffer();
+	 	//gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,this.ibuffer);
+	 	//gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint8Array(this.indexOrder),gl.STATIC_DRAW);
+	 	//gl.drawElements(gl.TRIANGLES,this.indexOrder.length,gl.UNSIGNED_BYTE,0);
+	 	gl.drawArrays(this.primitiveType, 0, this.triangleCount);
 	}
+
+	Update()
+	{
+		console.error(this.name +" update() is NOT IMPLEMENTED!");
+	}
+	
 }
+
+
+class Ground extends GameObject
+{
+	constructor()
+	{
+		super(4, gl.TRIANGLE_STRIP);
+		this.name = "Ground";
+		this.buffer=gl.createBuffer();
+		this.colorBuffer = gl.createBuffer();
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+		this.vertices =
+		[
+			-1000,0,-1000,0,1,0,
+			1000,0, -1000,0,1,0,
+			-1000,0,1000,0,1,0,
+			1000, 0,1000,0,1,0
+			
+		];
+	
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+		this.loc=[0,0,0];
+		this.rot=[0,0,0];
+	}
+	Update()
+	{
+		//Do Nothing
+	}
+	
+}
+
+
 class Hex extends GameObject
 {
 	constructor()
 	{
-		super();
+		super(18, gl.TRIANGLE_STRIP);
+		this.name = "Hex";
 		this.angVelocity = [0,.025,0];
-		this.isTrigger = false;
+		//this.isTrigger = false;
 		this.buffer=gl.createBuffer();
-		this.colorBuffer = gl.createBuffer();
+		//this.colorBuffer = gl.createBuffer();
 		
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
 		this.vertices =
@@ -208,77 +267,63 @@ class Hex extends GameObject
 	}
 	Update()
 	{
-		this.Move();
+		this.rot[1] += this.angVelocity[1];
 	}
-	Render(program)
-	{
-				var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-		var size = 3;          // 2 components per iteration
-		var type = gl.FLOAT;   // the data is 32bit floats
-		var normalize = false; // don't normalize the data
-		var stride = 6*Float32Array.BYTES_PER_ELEMENT;	//Size in bytes of each element     // 0 = move forward size * sizeof(type) each iteration to get the next position
-		var offset = 0;        // start at the beginning of the buffer
-		gl.enableVertexAttribArray(positionAttributeLocation);
-		gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
-		
-		//Now we have to do this for color
-		var colorAttributeLocation = gl.getAttribLocation(program,"vert_color");
-		//We don't have to bind because we already have the correct buffer bound.
-		size = 3;
-		type = gl.FLOAT;
-		normalize = false;
-		stride = 6*Float32Array.BYTES_PER_ELEMENT;	//Size in bytes of each element
-		offset = 3*Float32Array.BYTES_PER_ELEMENT;									//size of the offset
-		gl.enableVertexAttribArray(colorAttributeLocation);
-		gl.vertexAttribPointer(colorAttributeLocation, size, type, normalize, stride, offset);
-				
-		var tranLoc  = gl.getUniformLocation(program,'transform');
-		gl.uniform3fv(tranLoc,new Float32Array(this.loc));
-		var thetaLoc = gl.getUniformLocation(program,'rotation');
-		gl.uniform3fv(thetaLoc,new Float32Array(this.rot));
-	 
-	 //var ibuffer = gl.createBuffer();
-	 //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,this.ibuffer);
-	 //gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint8Array(this.indexOrder),gl.STATIC_DRAW);
-	 //gl.drawElements(gl.TRIANGLES,this.indexOrder.length,gl.UNSIGNED_BYTE,0);
-	 gl.drawArrays(gl.TRIANGLE_STRIP, 0, 18);
-	}
+	
 }
+
+
 class Camera extends GameObject
 {
 	constructor()
 	{
-			super();
+		super(0, gl.TRIANGLES);
+		this.name = "Camera";
+		this.loc = [0,0,0];
+		this.rot = [0,0,0];
 			
 	}
 	Update()
 	{
-		var deltaX = 0;
-		var deltaZ = 0;
-		var deltaR = 0;
-		if( "A" in m.Keys && m.Keys["A"])
+		// All of this is from the Player update
+		this.velocity = [0,0,0];
+		this.angVelocity = [0,0,0];
+		if("A" in m.Keys && m.Keys["A"])
 		{
-			this.rot[1] -=.01;
+			// 1 == rotate y
+			// If we want to rotate the camera to move
+			// right then i have to rotate the entire world to the
+			// left. So in this case we make sure to rotate counter-clockwise
+			this.angVelocity[1] -=.02;		//euler angles x,y,z
 		}
 		if("D" in m.Keys && m.Keys["D"])
 		{
-			this.rot[1] +=.01;
+			// Movign the world 
+			// Same idea of roation applies here! In this case to rotate the camera
+			// left, we make suere to rotate the world to the right (clock-wise)
+			this.angVelocity[1] +=.02;
 		}
-		if("W" in m.Keys && m.Keys["W"])
+		
+		// Make sure to rotate the direction vectors by the same ammount!
+		this.transform.doRotations(this.rot);
+		// Want to move in the z direction which is where the camera is pointing
+		var tempF = this.transform.forward;
+		if("W" in m.Keys && m.Keys["W"])	// Move forward 
 		{
-			this.transform.doRotations(this.rot);
-			deltaX += this.transform.forward[0]*.25;
-			deltaZ += this.transform.forward[2]*.25;
+			for(var i =0; i < 3; i++)
+			{
+				this.velocity[i] += tempF[i]*.1; 
+			}
 		}
-		if("S" in m.Keys && m.Keys["S"])
+		if("S" in m.Keys && m.Keys["S"])	// Move backwards
 		{
-			this.transform.doRotations(this.rot);
-			deltaX -= this.transform.forward[0]*.25;
-			deltaZ -= this.transform.forward[2]*.25;
+			for(var i =0; i < 3; i++)
+			{
+				this.velocity[i] -= tempF[i]*.1; 
+			}
 		}
-		this.loc[0] += deltaX;
-		this.loc[2] += deltaZ;
+
+		this.Move();
 	}
 	Render(program)
 	{
@@ -287,15 +332,23 @@ class Camera extends GameObject
 		var worldLoc = gl.getUniformLocation(program,'worldRotation');
 		gl.uniform3fv(worldLoc,new Float32Array(this.rot));
 	}
+
+	OnCollisionEnter(other)
+	{
+		if(other.name == "Hex")
+			console.log("Hex collided with camera");
+	}
 	
 	
 }
+
 
 class D4 extends GameObject
 {
 	constructor()
 	{
-		super();
+		super(12, gl.TRIANGLES);
+		this.name = "D4";
 		this.isTrigger = true;
 		this.buffer=gl.createBuffer();
 		 gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
