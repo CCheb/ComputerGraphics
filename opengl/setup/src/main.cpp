24a2c3f5afab43b442c9389b98bc12e940e8d6e0
Sebastian <amsebastian12@gmail.com>
2025-05-08 10:01:18 -0400

    added comments and color switching functionality

--- opengl/setup/src/main.cpp
+++ opengl/setup/src/main.cpp
@@ -1,57 +1,112 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <random>
 
-int main() {
-    // Internal error detection
-    glfwSetErrorCallback([](int error, const char* description) {
-        std::cerr << "GLFW Error (" << error << "): " << description << std::endl;
-    });
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
-    // Initialize GLFW 
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW\n";
-        return -1;
-    }
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-    // Setting up our context by specifying the opengl version that we want to use
-    // the current profile of openGL along with a macOS specific compatability line
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  // version 3.3
+// Background colors
+float R = 1.0f;
+float G = 1.0f;
+float B = 1.0f;
+
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL 3.3", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window\n";
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
-    // Make the context render at the window that we specify
     glfwMakeContextCurrent(window);
-    // Initializing glad by loading the location addresses of the OpenGL functions
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
-        return -1;
-    }
+    
+    // Function call back. Runs everytime the window is resized
+    // On mac you dont need this function; it resizes on its own
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // setting internal coordinates
-    // (-1,1) to (0,800) and (0,600)
-    glViewport(0,0,800,600);
-    // Window loop where we set the background and keep the window
-    // up until the user presses the exit button
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // Setting up color background. This has to be done in loop
+        glClearColor(R,G,B,1.0f);
+        // If you are changing colors then you need to clear the color buffer
         glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);    // Double buffer
+        // input
+        // -----
+        // An event thats listened too for each frame
+        processInput(window);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // clean up
-    glfwDestroyWindow(window);
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if(glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+    {
+        std::cout << "Space has been pressed!" << std::endl;
+
+        // Setting the random number generator
+        std::random_device rd;  // Seed
+        std::mt19937 gen(rd()); // Mersenne Twister RNG
+        std::uniform_real_distribution<float> dis(0.0f, 1.0f); // Range: [0.0, 1.0)
+
+        R = dis(gen);
+        G = dis(gen);
+        B = dis(gen);
+
+    }
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
