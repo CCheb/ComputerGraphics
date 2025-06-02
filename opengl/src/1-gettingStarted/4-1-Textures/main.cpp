3ba17df52c730806c6b852ec45d9fb75062e8e0f
Sebastian <amsebastian12@gmail.com>
2025-06-02 19:41:39 -0400

    Added combined textures section

--- opengl/src/1-gettingStarted/4-1-Textures/main.cpp
+++ opengl/src/1-gettingStarted/4-1-Textures/main.cpp
@@ -56,10 +56,10 @@ int main()
     // ------------------------------------------------------------------
     float vertices[] = {
         // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   3.0f, 3.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   3.0f, 0.0f, // bottom right
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 3.0f  // top left 
     };
     unsigned int indices[] = {  
         0, 1, 3, // first triangle
@@ -70,26 +70,29 @@ int main()
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
+    // Again, pressing the recording button on the recorder
     glBindVertexArray(VAO);
 
+    // VBO takes the verticies
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // EBO takes the indicies
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // position attribute
+    // position attribute (location 0)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // color attribute
+    // color attribute (location 1) skipping the first 3
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    // texture coord attribute
+    // texture coord attribute (location 2) skipping the first 6
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
 
-    // load and create a texture 
+    // load and create a texture. Its a similar process to vertex buffers
     // -------------------------
     unsigned int texture;
     glGenTextures(1, &texture);
@@ -98,22 +101,37 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    // GL_NEAREST produces sharper more pixelated images since its choosing the texel closest
+    // to the texture coordinate
+    // GL_LINEAR performs a texel average from the texels that are near the texture coordinate which 
+    // will produce a smoother but more clouded image (looses focus)
+
+    // You can also filter the mipmap itself using things like GL_LINEAR_MIPMAP_LINEAR etc.
+    // This is when you are going between mipmaps
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // Mipmaps are used when downscaling textures so we can omit setting a mipmap filtering to
+    // the magnification
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    std::string filePath = "resources/textures/container.jpg";
+    std::string filePath = "resources/textures/cobble.png";
+    // stbi_load will automatically fill in the width, height, and # of color channels fromt the image itself
     unsigned char *data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        // Some images are in RGBA formats while others might be in just RGB
+        // Attaching the image to the texture object bound to the global anchor point: GL_TEXTURE_2D
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        // Generate a collection of textures where each subsequent one is twice as small as the previous one
+        // OpenGL will decide which texture version to render based on distance
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
     {
         std::cout << "Failed to load texture" << std::endl;
     }
+    // Its good practice to free the image data after its been bounded
     stbi_image_free(data);
 
 
--- opengl/src/1-gettingStarted/4-1-Textures/texture.fs
+++ opengl/src/1-gettingStarted/4-1-Textures/texture.fs
@@ -4,10 +4,13 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-// texture sampler
+// texture sampler. This is what hold the texture itself
 uniform sampler2D texture1;
 
 void main()
 {
-	FragColor = 0.4*texture(texture1, TexCoord);
+	// Basically pairing the texture coordinated specified by the triangle 
+	// and passing them over to the texture itself so that the fragment shader can 
+	// interpolate the texture
+	FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);
 }
