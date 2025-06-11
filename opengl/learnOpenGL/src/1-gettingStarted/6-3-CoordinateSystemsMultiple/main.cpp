356c03dc24e0efee9ad224ecebc289c8b4232424
Sebastian <amsebastian12@gmail.com>
2025-06-11 09:21:48 -0400

    finished coordinate systems

--- opengl/learnOpenGL/src/1-gettingStarted/6-3-CoordinateSystemsMultiple/main.cpp
+++ opengl/learnOpenGL/src/1-gettingStarted/6-3-CoordinateSystemsMultiple/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
+#include <random>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -60,6 +61,8 @@ int main()
     Shader ourShader("./coordinateSystemsMul.vs", "./coordinateSystemsMul.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
+    // this set of verticies sets up a box with the same texture on each face
+    // each square = 2 triangles each with 3 vertices
     // ------------------------------------------------------------------
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -105,6 +108,10 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
     // world space positions of our cubes
+    // idea is to specify a location for each cube that will
+    // be rendered. notice how we are specifying positions outside of the
+    // -1.0 to 1.0. this is because we have setup a perspective matrix that
+    // handles the conversion to NDC coordinates.
     glm::vec3 cubePositions[] = {
         glm::vec3( 0.0f,  0.0f,  0.0f),
         glm::vec3( 2.0f,  5.0f, -15.0f),
@@ -121,6 +128,7 @@ int main()
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
+    // pressing the recording button
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -153,7 +161,7 @@ int main()
     std::string filePath = "resources/textures/container.jpg";
     unsigned char *data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
     if (data)
-    {
+    {   // Copy the data over to the texture object and generate mipmaps after that
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
@@ -162,7 +170,7 @@ int main()
         std::cout << "Failed to load texture" << std::endl;
     }
     stbi_image_free(data);
-    // texture 2
+    // texture 2 same process as in texture 1
     // ---------
     glGenTextures(1, &texture2);
     glBindTexture(GL_TEXTURE_2D, texture2);
@@ -194,6 +202,12 @@ int main()
     ourShader.setInt("texture2", 1);
 
 
+    std::random_device rd;                         // Seed
+    std::mt19937 gen(rd());                        // Mersenne Twister engine
+    std::uniform_real_distribution<> distr(0.0f, 100.0f); // Range: [1, 100)
+    
+    float randNum = distr(gen);
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -208,18 +222,20 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
          // bind textures on corresponding texture units
+         // think of this step as passing the texture over to the sampler
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        // activate shader
+        // activate shader (optional since we enabled our program before the loop)
         ourShader.use();
 
         // create transformations
         glm::mat4 view          = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        glm::mat4 projection    = glm::mat4(1.0f);
+        glm::mat4 projection    = glm::mat4(1.0f); // with identity matrix
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        // same as moving the camera back 3 units. To do this we have to move the world three units in the -z direction
         view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         // pass transformation matrices to the shader
         ourShader.setMat4("projection", projection); // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
@@ -230,12 +246,14 @@ int main()
         for (unsigned int i = 0; i < 10; i++)
         {
             // calculate the model matrix for each object and pass it to shader before drawing
+            // each object must be updated and drawn before the next object is handled
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
+            float angle = randNum * i;
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
 
+            
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
